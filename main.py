c399e4c3e2b66c6d16559a4be02130e0e485305e
Brayan <144967324+BrayanOmar0508@users.noreply.github.com>
2025-05-20 21:59:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def show_guilds(ctx):
 async def change_language(ctx, lang_code: str):
     lang_code = lang_code.lower()
     if lang_code not in ["en", "fr"]:
-        await ctx.send("❌ Invalid language. Available: en, fr")
+        await ctx.send("❌ Invalid language. Available: `en`, `fr`")
         return
 
     user_languages[ctx.author.id] = lang_code
@@ -64,8 +64,8 @@ async def check_ban_command(ctx):
 
     if not user_id.isdigit():
         message = {
-            "en": f"{ctx.author.mention} ❌ **Invalid UID!**\n➡️ Please use: !check 123456789",
-            "fr": f"{ctx.author.mention} ❌ **UID invalide !**\n➡️ Veuillez fournir un UID valide sous la forme : !check 123456789"
+            "en": f"{ctx.author.mention} ❌ **Invalid UID!**\n➡️ Please use: `!ID 123456789`",
+            "fr": f"{ctx.author.mention} ❌ **UID invalide !**\n➡️ Veuillez fournir un UID valide sous la forme : `!ID 123456789`"
         }
         await ctx.send(message[lang])
         return
@@ -74,9 +74,7 @@ async def check_ban_command(ctx):
         try:
             ban_status = await check_ban(user_id)
         except Exception as e:
-            await ctx.send(f"{ctx.author.mention} ⚠️ Error:\n
-{str(e)}
-")
+            await ctx.send(f"{ctx.author.mention} ⚠️ Error:\n```{str(e)}```")
             return
 
         if ban_status is None:
@@ -91,7 +89,7 @@ async def check_ban_command(ctx):
         period = ban_status.get("period", "N/A")
         nickname = ban_status.get("nickname", "NA")
         region = ban_status.get("region", "N/A")
-        id_str = f"{user_id}"
+        id_str = f"`{user_id}`"
 
         if isinstance(period, int):
             period_str = f"more than {period} months" if lang == "en" else f"plus de {period} mois"
@@ -109,24 +107,28 @@ async def check_ban_command(ctx):
                 f"**• {'Reason' if lang == 'en' else 'Raison'} :** "
                 f"{'This account was confirmed for using cheats.' if lang == 'en' else 'Ce compte a été confirmé comme utilisant des hacks.'}\n"
                 f"**• {'Suspension duration' if lang == 'en' else 'Durée de la suspension'} :** {period_str}\n"
-                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** {nickname}\n"
-                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** {id_str}\n"
-                f"**• {'Region' if lang == 'en' else 'Région'} :** {region}"
+                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
+                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
+                f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
             )
-            embed.set_image(url="https://i.ibb.co/wFxTy8TZ/banned.gif")
+            # embed.set_image(url="https://i.ibb.co/wFxTy8TZ/banned.gif")
+            file = discord.File("assets/banned.gif", filename="banned.gif")
+            embed.set_image(url="attachment://banned.gif")
         else:
             embed.title = "**▌ Clean Account ✅ **" if lang == "en" else "**▌ Compte non banni ✅ **"
             embed.description = (
                 f"**• {'Status' if lang == 'en' else 'Statut'} :** "
                 f"{'No sufficient evidence of cheat usage on this account.' if lang == 'en' else 'Aucune preuve suffisante pour confirmer l’utilisation de hacks sur ce compte.'}\n"
-                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** {nickname}\n"
-                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** {id_str}\n"
-                f"**• {'Region' if lang == 'en' else 'Région'} :** {region}"
+                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
+                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
+                f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
             )
-            embed.set_image(url="https://i.ibb.co/Kx1RYVKZ/notbanned.gif")
+            # embed.set_image(url="https://i.ibb.co/Kx1RYVKZ/notbanned.gif")
+            file = discord.File("assets/notbanned.gif", filename="notbanned.gif")
+            embed.set_image(url="attachment://notbanned.gif")
 
         embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
         embed.set_footer(text="📌  Garena Free Fire")
-        await ctx.send(f"{ctx.author.mention}", embed=embed)
+        await ctx.send(f"{ctx.author.mention}", embed=embed ,file=file)
 
 bot.run(TOKEN)
