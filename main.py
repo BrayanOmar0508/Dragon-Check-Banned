f8b389a665103c481c7e461481d81ee41498c9ff
Brayan <144967324+BrayanOmar0508@users.noreply.github.com>
2025-05-20 21:16:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 DEFAULT_LANG = "en"
 user_languages = {}
